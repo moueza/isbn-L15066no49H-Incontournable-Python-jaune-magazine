ba05d40ea46976ed5a3180354d86a6720d200203
moueza <>
2022-10-23 09:19:00 +0200

    see #1 OK ; Resolves #3 Tuples

---
+++ TUPLES/tuples.py
@@ -0,0 +1,10 @@
+#https://www.w3schools.com/python/python_tuples_access.asp
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+print(len(thistuple))
+
+
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
+
+print(thistuple[1])
